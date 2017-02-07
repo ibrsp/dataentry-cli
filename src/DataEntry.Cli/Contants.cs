ab6973622ff0bd2898ac436ce33c3753c4e1ea56
Eugene Sergueev <eugene.sergueev@gmail.com>
2017-02-07 13:55:38 +0300

    feat: create a skeleton for CLI project

---
+++ src/DataEntry.Cli/Contants.cs
@@ -0,0 +1,22 @@
+﻿namespace DataEntry.Cli
+{
+    internal static class Constants
+    {
+        internal static class CommandOptions
+        {
+            internal const string HelpOption = "-? | -h | --help";
+        }
+
+        internal static class ExitCodes
+        {
+            internal const int Bad = 0xbad;
+            internal const int Ok = 0;
+        }
+
+        internal static class ApiSegments
+        {
+            public const string Api = "api";
+            public const string Sequence = "sequence";
+        }
+    }
+}
--- src/DataEntry.Cli/Program.cs
+++ src/DataEntry.Cli/Program.cs
@@ -1,15 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using Microsoft.Extensions.CommandLineUtils;
 
 namespace DataEntry.Cli
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private Program()
         {
+        }
+
+        private static int Main(string[] args)
+        {
+            var program = new Program();
+            return program.Run(args);
+        }
+
+        private int Run(string[] args)
+        {
+            var app = new CommandLineApplication(false)
+            {
+                Name = "de",
+                Description = "Set of tools to autome routine tasks",
+                FullName = "data entry toolbox"
+            };
+
+            app.HelpOption(Constants.CommandOptions.HelpOption);
+            app.VersionOption("--version",
+                () => typeof(Program)
+                    .GetTypeInfo()
+                    .Assembly
+                    .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                    .InformationalVersion);
+
+
+            app.OnExecute(() =>
+            {
+                app.ShowHelp();
+                return Constants.ExitCodes.Ok;
+            });
+
+            try
+            {
+                return app.Execute(args);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+
+                (ex as CommandParsingException)?.Command.ShowHelp();
+            }
+
+            return Constants.ExitCodes.Bad;
         }
     }
 }
