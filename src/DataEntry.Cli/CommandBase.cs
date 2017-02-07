34be74ace4813f6168504c8318af21e478c59072
Eugene Sergueev <eugene.sergueev@gmail.com>
2017-02-07 13:59:54 +0300

    feat: define a set of helper classes for configure CLI subcommands

---
+++ src/DataEntry.Cli/CommandBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace DataEntry.Cli
+{
+    internal abstract class CommandBase
+    {
+        public abstract void Register(CommandLineApplication app);
+
+        protected virtual void DefineArgumentsAndOptions(CommandLineApplication cmd)
+        {
+            cmd.HelpOption(Constants.CommandOptions.HelpOption);
+        }
+
+        protected void Configure(CommandLineApplication cmd)
+        {
+            DefineArgumentsAndOptions(cmd);
+
+            cmd.OnExecute(() =>
+            {
+                cmd.ShowRootCommandFullNameAndVersion();
+                try
+                {
+                    Validate();
+                }
+                catch (ArgumentException ex)
+                {
+                    throw new CommandParsingException(cmd, ex.Message);
+                }
+
+                return Execute();
+            });
+        }
+
+        protected virtual void Validate()
+        {
+        }
+
+        protected virtual int Execute()
+        {
+            return Constants.ExitCodes.Ok;
+        }
+    }
+}
---
+++ src/DataEntry.Cli/CommandLineApplicationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+
+namespace DataEntry.Cli
+{
+    internal static class CommandLineApplicationExtensions
+    {
+        public static CommandLineApplication RegisterCommand<TCommand>(this CommandLineApplication app)
+            where TCommand : CommandBase, new()
+        {
+            var command = new TCommand();
+            command.Register(app);
+            return app;
+        }
+    }
+}
